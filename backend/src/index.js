85cb01face477cabeb893a8bf3ba0eb375c4c678
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-06-21 22:28:44 +0500

    complete mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/dbConnect.js";
+import app from "./app.js";
 
 dotenv.config({
     path: "./.env",
 });
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => [
+            console.log(`Server is Listening on: ${process.env.PORT}`),
+        ]);
+    })
+    .catch((error) => {
+        console.log("MONGODB connection failed", error);
+    });
