45b56d64e0baccad4cf19db03edcf081ffd18282
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-06-23 12:46:28 +0500

    add user, video  schema

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,45 @@
+import { model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary
+            required: [true, "Video file is required"],
+        },
+        thumbnail: {
+            type: String, // cloudinary
+            required: [true, "Thumbnail is required"],
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: [true, "Description is required"],
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            required: [true, "Duration is required"],
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = model("Video", videoSchema);
