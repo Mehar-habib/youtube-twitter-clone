0d39b87cd4d74199d4548fff632e2640ac5b477b
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-04 21:24:06 +0500

    add a comment to  a video

--- backend/src/controller/comment.controller.js
+++ backend/src/controller/comment.controller.js
@@ -79,4 +79,32 @@ const getVideoComments = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, comments, "Comments fetched successfully"));
 });
 
-export { getVideoComments };
+// ! add a comment to a video
+const addComment = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req.user?._id,
+    });
+    if (!comment) {
+        throw new ApiError(500, "Failed to add comment please try again");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "Comment added successfully"));
+});
+
+export { getVideoComments, addComment };
