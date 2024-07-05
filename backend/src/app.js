4f09f899e846849d21a6a3524ed540eaf692a55d
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-05 22:24:43 +0500

    make routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 const app = new express();
 app.use(
@@ -20,5 +21,6 @@ app.use(cookieParser());
 // User Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/likes", likeRouter);
 
 export default app;
