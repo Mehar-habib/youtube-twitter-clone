a51473009e654d159ba20158bba4bdc873d7e734
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-08 11:42:24 +0500

    updateTweet

--- backend/src/controller/tweet.controller.js
+++ backend/src/controller/tweet.controller.js
@@ -22,4 +22,42 @@ const createTweet = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, tweet, "Tweet created successfully"));
 });
 
-export { createTweet };
+const updateTweet = asyncHandler(async (req, res) => {
+    const { content } = req.body;
+    const { tweetId } = req.params;
+
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id");
+    }
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(400, "Tweet not found");
+    }
+
+    if (tweet?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "You are not authorized to update this tweet");
+    }
+
+    const newTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content,
+            },
+        },
+        { new: true }
+    );
+
+    if (!newTweet) {
+        throw new ApiError(400, "Failed to update tweet");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, newTweet, "Tweet updated successfully"));
+});
+
+export { createTweet, updateTweet };
