065434852524fc51791cb6d5ed0d1ae249b0a5f9
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-08 21:05:03 +0500

    make playlist routes endpoint and fix the bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import healthRouter from "./routes/healthcheck.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 const app = new express();
 app.use(
@@ -30,5 +31,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/healthcheck", healthRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
 export default app;
--- backend/src/controller/playlist.controller.js
+++ backend/src/controller/playlist.controller.js
@@ -3,7 +3,7 @@ import { Playlist } from "../models/playlist.model.js";
 import { Video } from "../models/video.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import asyncHandler from "../utils/asyncHandle.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 // ! create playlist
 const createPlaylist = asyncHandler(async (req, res) => {
