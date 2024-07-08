e64ed60b3af2156aec350950e845b74de868f35b
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-08 15:04:49 +0500

    publish a video

--- backend/src/controller/video.controller.js
+++ backend/src/controller/video.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { Video } from "../models/video.model.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 // !get all videos based on query, sort, pagination
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -65,4 +66,54 @@ const getAllVideos = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, video, "Videos fetched successfully"));
 });
 
-export { getAllVideos };
+// ! get video, upload to cloudinary, create video
+const publishAVideo = asyncHandler(async (req, res) => {
+    const { title, description } = req.body;
+    if ([title, description].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+    const videoFileLocalPath = req.files?.videoFile[0].path;
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+    if (!videoFileLocalPath) {
+        throw new ApiError(400, "Video file is required");
+    }
+    if (!thumbnailLocalPath) {
+        throw new ApiError(400, "Thumbnail file is required");
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    if (!videoFile) {
+        throw new ApiError(400, "Video file not found");
+    }
+    if (!thumbnail) {
+        throw new ApiError(400, "Thumbnail not found");
+    }
+
+    const video = await Video.create({
+        title,
+        description,
+        duration: videoFile.duration,
+        videoFile: {
+            url: videoFile.url,
+            public_id: videoFile.public_id,
+        },
+        thumbnail: {
+            url: thumbnail.url,
+            public_id: thumbnail.public_id,
+        },
+        owner: req.user._id,
+    });
+
+    const videoUploaded = await Video.findById(video._id);
+    if (!videoUploaded) {
+        throw new ApiError(400, "videoUpload failed please try again !!!");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Video uploaded successfully"));
+});
+
+export { getAllVideos, publishAVideo };
