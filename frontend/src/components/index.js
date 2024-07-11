56bae131c5ecc98ab9e0780262333a9f05dcb726
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-11 23:34:20 +0500

    organizing components and icons

---
+++ frontend/src/components/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Container from "./Container";
+import Input from "./Input";
+import Login from "./Login";
+import Navbar from "./header/Navbar";
+import Search from "./header/Search";
+import Sidebar from "./header/Sidebar";
+import Logo from "./Logo";
+
+export { Button, Container, Input, Login, Navbar, Search, Sidebar, Logo };
