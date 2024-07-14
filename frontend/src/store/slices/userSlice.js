9eb206d4615b31bfa8b306c06d4c12f6fe2fc829
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2024-07-14 14:51:47 +0500

    add updateUserDetails, updateAvatar, updateCoverImg slices

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -31,6 +31,48 @@ export const getWatchHistory = createAsyncThunk("getWatchHistory", async () => {
   }
 });
 
+export const updateUserDetails = createAsyncThunk(
+  "updateUserDetails",
+  async (data) => {
+    try {
+      const response = await axiosInstance.patch("/users/update-user", data);
+      toast.success("Updated details successfully!!!");
+      return response.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
+export const updateAvatar = createAsyncThunk("updateAvatar", async (avatar) => {
+  try {
+    const response = await axiosInstance.patch("/users/update-avatar", avatar);
+    toast.success("Updated details successfully!!!");
+    return response.data.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.error);
+    throw error;
+  }
+});
+
+export const updateCoverImg = createAsyncThunk(
+  "updateCoverImg",
+  async (coverImage) => {
+    try {
+      const response = await axiosInstance.patch(
+        "/users/update-coverImg",
+        coverImage
+      );
+      toast.success(response.data?.message);
+      return response.data.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -53,6 +95,27 @@ const userSlice = createSlice({
     builder.addCase(getWatchHistory.fulfilled, (state, action) => {
       state.loading = false;
       state.history = action.payload;
+    });
+    builder.addCase(updateUserDetails.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(updateUserDetails.fulfilled, (state, action) => {
+      state.loading = false;
+      state.profileData = action.payload;
+    });
+    builder.addCase(updateAvatar.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(updateAvatar.fulfilled, (state, action) => {
+      state.loading = false;
+      state.profileData = action.payload;
+    });
+    builder.addCase(updateCoverImg.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(updateCoverImg.fulfilled, (state, action) => {
+      state.loading = false;
+      state.profileData = action.payload;
     });
   },
 });
